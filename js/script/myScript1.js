693fc2b2efdfa11929d83322a36755d4739f85a8
digivcam24 <vimeanseththorngstf@gmail.com>
2024-04-04 13:59:54 +0900

    ...

---
+++ js/script/myScript1.js
@@ -0,0 +1,4 @@
+function myFunction1() {
+    document.getElementById("demo2").innerHTML = "Paragraph changed - myScript (1).";
+    document.getElementById("demo2").style.color="red";
+  }
---
+++ js/script/myScript2.js
@@ -0,0 +1,4 @@
+function myFunction2() {
+    document.getElementById("demo2").innerHTML = "Paragraph changed - myScript (2).";
+    document.getElementById("demo2").style.color="blue";
+  }
